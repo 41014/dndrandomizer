ccf3512021284902224158150ca9b3caba24a24d
41014 <59745283+41014@users.noreply.github.com>
2020-02-16 23:14:59 -0600

    Update dnd.py
    defined the randomize() function that randomizes integers in a range from 1 to 20. added Entry widgets that hold the randomized values. On click, the Randomize button checks to see if the Entry has something in it, and replaces it with the new value.

--- dnd.py
+++ dnd.py
@@ -1,67 +1,73 @@
 from tkinter import *
 import random
 
-root = Tk()
-root.title("DnD")
-
-
-#defining functions
 
 def randomize():
-    return
+    if resultStrength.index("end") != 0:
+        resultStrength.delete(0, END)
+    if resultDexterity.index("end") !=0:
+        resultDexterity.delete(0, END)
+    if resultIntelligence.index("end") !=0:
+        resultIntelligence.delete(0, END)
+    if resultWisdom.index("end") !=0:
+        resultWisdom.delete(0, END)
+    if resultCharisma.index("end") !=0:
+        resultCharisma.delete(0, END)
 
-def race():
-    return
+    statStr = random.randint(1, 20)
+    statDex = random.randint(1, 20)
+    statInt = random.randint(1, 20)
+    statWis = random.randint(1, 20)
+    statChar = random.randint(1, 20)
+    resultStrength.insert(0, statStr)
+    resultDexterity.insert(0, statDex)
+    resultIntelligence.insert(0, statInt)
+    resultWisdom.insert(0, statWis)
+    resultCharisma.insert(0, statChar)
 
-def strength():
-    return
-
-def dexterity():
-    return
-
-def intelligence():
-    return
-
-def wisdom():
-    return
-
-def charisma():
-    return
+root = Tk()
+root.title("D&D Rndmzr")
 
 
 #frame stuff
-frame = LabelFrame(root, text="Create your character", padx = 50, pady = 40)
+frame = LabelFrame(root, text="Randomize Your Character", padx = 50, pady = 40)
 frame.grid(padx=10, pady=10)
 
 
 #Random All
-buttonRandomize = Button(frame, text="Randomize Stats", command= lambda: randomize())
+buttonRandomize = Button(frame, text="Randomize Stats", command=lambda: randomize())
 buttonRandomize.grid(row=0, column=0)
 
 
-#Name
-labelName = Label(frame, text="Name")
-labelName.grid(row=1, column=0)
-
 #Strength
 labelStrength = Label(frame, text="Strength")
-labelStrength.grid(row=3, column=0)
+labelStrength.grid(row=1, column=0)
+resultStrength = Entry(frame)
+resultStrength.grid(row=2, column=0)
 
 #Dexterity
 labelDexterity = Label(frame, text="Dexterity")
-labelDexterity.grid(row=5, column=0)
+labelDexterity.grid(row=3, column=0)
+resultDexterity = Entry(frame)
+resultDexterity.grid(row=4, column=0)
 
 #Intelligence
 labelIntelligence = Label(frame, text="Intelligence")
-labelIntelligence.grid(row=7, column=0)
+labelIntelligence.grid(row=5, column=0)
+resultIntelligence = Entry(frame)
+resultIntelligence.grid(row=6, column=0)
 
 #Wisdom
 labelWisdom = Label(frame, text="Wisdom")
-labelWisdom.grid(row=9, column=0)
+labelWisdom.grid(row=7, column=0)
+resultWisdom = Entry(frame)
+resultWisdom.grid(row=8, column=0)
 
 #Charisma
 labelCharisma = Label(frame, text="Charisma")
-labelCharisma.grid(row=11, column=0)
+labelCharisma.grid(row=9, column=0)
+resultCharisma = Entry(frame)
+resultCharisma.grid(row=10, column=0)
 
 
 root.resizable(width=False, height=False)
