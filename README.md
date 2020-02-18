1296b8adfab715b8d07e576eae86b4370188f22c
41014 <59745283+41014@users.noreply.github.com>
2020-02-17 19:25:50 -0600

    final changes to dndrndmzr
    Added a state-change to the Entry box. Prevents the user from editing the results of the randomized roll. Also added the Constitution stat... I'd completely forgotten it.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # dndrandomizer
- Randomizes the stats for a D&D character
+
+Randomizes the stat rolls for a D&D character without needing dice!
+
+monday feb 17, 2020//
+I don't know what I'm supposed to put in a README yet since I'm still new to coding, so I guess I'll put some of my thoughts about the project now that I'm done with it (for the moment).
+
+I wanted to create something that I thought would be fairly simple to make as a beginner and might be useful to me in some way. I needed for there to be a GUI since I'm in love with the idea of making an application that I could give to someone else and allow them to easily use.
+
+Tkinter was my only option, as I didn't want to venture off too far away from the bare essentials. Through developing this, I was able to familiarize myself with the scope of what one function could pack into a program, and just how long they could be.
+
+The randomize() function inside of the "Roll" button first fixes the state of each Entry widget to "normal." Next, it goes through "if" statements assigned to each widget - if the Entry is not empty, then it will delete the contents. This check is in place because otherwise, the function will concatenate the number on each roll, making the result grow.
+    Example:
+        Strength first roll (14) - 14
+        Strength second roll (10) - 1014
+        Strength third roll (4) - 41014
+
+After that, each category has a variable that is assigned the result of random.randint(1, 20). The range of 1-20 was chosen because the die used in Dungeons and Dragons to roll stats is the d20. Those variables are then inserted into each Entry widget and the numbers are displayed.
+
+Finally, the Entry widgets' states are changed to "readonly," which prevents the randomized values in the boxes from being changed by the user. Pressing the "Roll" button again restarts the function, which changes the widgets back to the "normal" state. The widgets have to be changed from "normal" to "readonly" and back because Entry widgets in a "readonly" state do not update, but the changes are reflected if the state is "normal." Unfortunately, "normal" allows the user to type in the Entry widgets and change the results, which is undesireable behavior, so the widgets have to go back into the "readonly" state after the numbers are generated and displayed.
+
+
+Looking at the code now that it's done, I feel like I could have used lists and loops to add methods and change states of widgets somehow, but I'm still not comfortable or confident when it comes to using loops. Having every single thing typed out line-for-line without a loop does provide me some clarity.
--- dnd.py
+++ dnd.py
@@ -3,10 +3,19 @@ import random
 
 
 def randomize():
+    resultStrength.configure(state ="normal")
+    resultDexterity.configure(state="normal")
+    resultConstitution.configure(state="normal")
+    resultIntelligence.configure(state="normal")
+    resultWisdom.configure(state="normal")
+    resultCharisma.configure(state="normal")
+
     if resultStrength.index("end") != 0:
         resultStrength.delete(0, END)
     if resultDexterity.index("end") !=0:
         resultDexterity.delete(0, END)
+    if resultConstitution.index("end") !=0:
+        resultConstitution.delete(0, END)
     if resultIntelligence.index("end") !=0:
         resultIntelligence.delete(0, END)
     if resultWisdom.index("end") !=0:
@@ -16,58 +25,73 @@ def randomize():
 
     statStr = random.randint(1, 20)
     statDex = random.randint(1, 20)
+    statCon = random.randint(1, 20)
     statInt = random.randint(1, 20)
     statWis = random.randint(1, 20)
     statChar = random.randint(1, 20)
+
     resultStrength.insert(0, statStr)
     resultDexterity.insert(0, statDex)
+    resultConstitution.insert(0, statCon)
     resultIntelligence.insert(0, statInt)
     resultWisdom.insert(0, statWis)
     resultCharisma.insert(0, statChar)
 
+    resultStrength.configure(state="readonly", readonlybackground="white")
+    resultDexterity.configure(state="readonly", readonlybackground="white")
+    resultConstitution.configure(state = "readonly", readonlybackground="white")
+    resultIntelligence.configure(state="readonly", readonlybackground="white")
+    resultWisdom.configure(state="readonly", readonlybackground="white")
+    resultCharisma.configure(state="readonly", readonlybackground="white")
+
+
+
 root = Tk()
-root.title("D&D Rndmzr")
+root.title("DnD Rndmzr")
 
-
-#frame stuff
-frame = LabelFrame(root, text="Randomize Your Character", padx = 50, pady = 40)
+#Frame Stuff
+frame = LabelFrame(root, text="Roll Your Character", padx = 50, pady = 40)
 frame.grid(padx=10, pady=10)
 
-
 #Random All
-buttonRandomize = Button(frame, text="Randomize Stats", command=lambda: randomize())
+buttonRandomize = Button(frame, text="Roll", command=lambda: randomize())
 buttonRandomize.grid(row=0, column=0)
-
 
 #Strength
 labelStrength = Label(frame, text="Strength")
 labelStrength.grid(row=1, column=0)
-resultStrength = Entry(frame)
+resultStrength = Entry(frame, justify = "center")
 resultStrength.grid(row=2, column=0)
 
 #Dexterity
 labelDexterity = Label(frame, text="Dexterity")
 labelDexterity.grid(row=3, column=0)
-resultDexterity = Entry(frame)
+resultDexterity = Entry(frame, justify = "center")
 resultDexterity.grid(row=4, column=0)
+
+#Constitution
+labelConstitution = Label(frame, text="Constitution")
+labelConstitution.grid(row=5, column=0)
+resultConstitution = Entry(frame, justify = "center")
+resultConstitution.grid(row=6, column=0)
 
 #Intelligence
 labelIntelligence = Label(frame, text="Intelligence")
-labelIntelligence.grid(row=5, column=0)
-resultIntelligence = Entry(frame)
-resultIntelligence.grid(row=6, column=0)
+labelIntelligence.grid(row=7, column=0)
+resultIntelligence = Entry(frame, justify = "center")
+resultIntelligence.grid(row=8, column=0)
 
 #Wisdom
 labelWisdom = Label(frame, text="Wisdom")
-labelWisdom.grid(row=7, column=0)
-resultWisdom = Entry(frame)
-resultWisdom.grid(row=8, column=0)
+labelWisdom.grid(row=9, column=0)
+resultWisdom = Entry(frame, justify = "center")
+resultWisdom.grid(row=10, column=0)
 
 #Charisma
 labelCharisma = Label(frame, text="Charisma")
-labelCharisma.grid(row=9, column=0)
-resultCharisma = Entry(frame)
-resultCharisma.grid(row=10, column=0)
+labelCharisma.grid(row=11, column=0)
+resultCharisma = Entry(frame, justify = "center")
+resultCharisma.grid(row=12, column=0, )
 
 
 root.resizable(width=False, height=False)
